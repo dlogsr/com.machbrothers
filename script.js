cd638a8c5772dba875dc13852fdc88247faf9e25
Ryan Dumlao <dlogsr@gmail.com>
2014-07-02 01:59:35 -0700

    mobile cards, desktop flat design. added founder's portraits

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+function scrollAndStop(marker){
+	var scrollOffset;
+	console.log(marker);
+	scrollOffset = $(marker).offset().top - $('.mbHeader').height();
+	$('html,body').stop().animate({scrollTop : scrollOffset},400);
+}
+
+var clickedLink;
+
 $(document).ready(function(){
 	$('.mbNavicon').click(function(){
 		$('.mbLinks').slideToggle();
 	});
+
+	$('.mbLogo').click(function(e){
+		e.preventDefault();
+		if(!window.matchMedia("(min-width: 680px)").matches)	$('.mbLinks').slideToggle();
+		scrollAndStop('body');
+	});
+
+	$('.mbLinkButton').click(function(e){
+		e.preventDefault();
+		if(!window.matchMedia("(min-width: 680px)").matches)	$('.mbLinks').slideToggle();
+		clickedLink = $(this).attr('id');
+		clickedLink = '.' + clickedLink + 'Card';
+		scrollAndStop(clickedLink);
+	})
 
 	if(window.matchMedia("(min-width: 680px)").matches) $(window).stellar();
 });
