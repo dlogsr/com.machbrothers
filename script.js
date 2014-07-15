0abe053cb6bc9b080bcf116797ab9b1a6440b2a6
Ryan Dumlao <dlogsr@gmail.com>
2014-07-15 13:23:02 -0700

    desktop view menu translucency

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-function scrollAndStop(marker){
-	var scrollOffset;
-	console.log(marker);
-	scrollOffset = $(marker).offset().top - $('.mbHeader').height();
-	$('html,body').stop().animate({scrollTop : scrollOffset},400);
-}
+
+$menuBar = $('.menuBar');
+$mbLinks = $('.mbLinks');
+$mbLogo = $('.mbLogo');
+$mbTranslucent = $('.mbTranslucent');
+$mbNavicon = $('.mbNavicon');
+$mbHeader = $('.mbHeader');
+$pageContent = $('.pageContent');
 
 var clickedLink;
 
+function scrollAndStop(marker){
+	var scrollOffset;
+	console.log(marker);
+	scrollOffset = $(marker).offset().top - $mbHeader.height();
+	$('html,body').stop().animate({scrollTop : scrollOffset},400);
+}
+
 $(document).ready(function(){
-	$('.mbNavicon').click(function(){
-		$('.mbLinks').slideToggle();
+	$mbNavicon.click(function(){
+		$mbLinks.slideToggle();
 	});
 
-	$('.mbLogo').click(function(e){
+	$mbLogo.click(function(e){
 		e.preventDefault();
-		if(!window.matchMedia("(min-width: 680px)").matches)	$('.mbLinks').slideToggle();
+		if(!window.matchMedia("(min-width: 680px)").matches)	$mbLinks.slideToggle();
 		scrollAndStop('body');
 	});
 
@@ -29,3 +38,31 @@ $(document).ready(function(){
 
 	if(window.matchMedia("(min-width: 680px)").matches) $(window).stellar();
 });
+
+isFullScreen = window.matchMedia("(min-width: 900px)").matches;
+
+$pageContent.waypoint(function(direction){
+	if(window.matchMedia("(min-width:680px)").matches)
+	{
+		if(direction == 'down'){
+			$menuBar.addClass('mbTranslucent');
+		}
+		else{
+			$menuBar.add($mbLinks).add($mbLogo).removeClass('mbTranslucent');
+		}
+	}
+
+},{
+	offset:55
+});
+
+// $pageContent.waypoint(function(direction){
+// 	if(window.matchMedia("(min-width:680px)").matches)
+// 	{
+// 		if(direction == 'up'){
+// 			$menuBar.add($mbLinks).add($mbLogo).removeClass('mbTranslucent');
+// 		}
+// 	}
+// },{
+// 	offset:55
+// });
