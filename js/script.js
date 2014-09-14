80a760b68b3fa35561f7b5af841f83eacf9d12b0
Ryan Dumlao <dlogsr@gmail.com>
2014-09-13 18:45:08 -0700

    ongoin fix for dragon positioning on iOS mobile safari landscape (non-mobile view)

--- js/script.js
+++ js/script.js
@@ -59,7 +59,6 @@ $(document).ready(function(){
 			$(window).stellar({
 				horizontalScrolling: false
 			});
-			$('#debug').html('test');
 		};
 		// if(window.matchMedia("(min-width: 768px)").matches) $pageSplashDragon.stellar({
 		// 	scrollProperty: 'transform'
